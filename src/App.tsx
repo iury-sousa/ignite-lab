e7ae54725f5ebea82d8da028adabbe2726fecda1
IuryWemerson <iury_wemerson@hotmail.com>
2022-06-21 23:37:35 -0300

    creates components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react";
-import { client } from "./lib/apollo";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event";
 
 export function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
-  console.log(data);
-  return <h1>Hello World!</h1>;
+  return <Event />;
 }
